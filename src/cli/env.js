faee57309f4ac067160e4f9917d4e5c5cc2da943
ImmelstronDev <wishandra.afk@gmail.com>
2024-01-29 16:36:45 +0300

    feat: complement parseEnv in env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+const prefix = 'RSS_'
+const envKeys = Object.keys(process.env)
+
 const parseEnv = () => {
     // Write your code here 
+    // console.log(envKeys)
+    const resultArr = envKeys.reduce((acc, cur) => {
+        if(cur.indexOf(prefix) === 0) {
+            const res = `${cur}=${process.env[cur]}`
+            acc.push(res)
+            return acc
+        }
+        return acc
+    },[])
+    const result = resultArr.join('; ')
+    console.log(result)
 };
 
 parseEnv();
