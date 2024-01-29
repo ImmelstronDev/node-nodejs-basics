65c8ccee342cae83c48b61ed44ef08febc657671
ImmelstronDev <wishandra.afk@gmail.com>
2024-01-29 17:02:12 +0300

    feat: complement parseArgs function args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+const separator = '--'
+
 const parseArgs = () => {
     // Write your code here 
+    const commandArgs = process.argv.slice(2)
+    // console.log(commandArgs)
+    const resultArgs = commandArgs.reduce((acc, cur, index)=> {
+        if(cur.indexOf(separator) === 0 ) {
+            acc.push(`${cur.slice(2)} is ${commandArgs[index+1]}`)
+            return acc
+        }
+        return acc
+    }, [])
+    console.log(resultArgs.join(', '))
 };
 
 parseArgs();
