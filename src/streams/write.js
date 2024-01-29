29190e4a1e6fb6f7700b856ea7eb36b1ff0c4cb6
ImmelstronDev <wishandra.afk@gmail.com>
2024-01-29 19:20:04 +0300

    feat: complement write function in write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'node:fs'
+import {dirname, resolve} from 'node:path'
+import { stdin } from 'node:process'
+import {fileURLToPath} from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const __path = resolve(__dirname, 'files', 'fileToWrite.txt')
+
 const write = async () => {
     // Write your code here 
+    const writeStream = createWriteStream(__path)
+    stdin.pipe(writeStream)
 };
 
 await write();
